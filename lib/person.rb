a5a60a8734f704be4421a3b76a19dad199947b1e
Tyler Hunt <tyhunt04@gmail.com>
2019-02-28 18:36:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  
+
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
@@ -8,5 +8,5 @@ class Person
     attributes.each do |key, value|
       self.send(("#{key}="), value)
   end
-  
+
 end
